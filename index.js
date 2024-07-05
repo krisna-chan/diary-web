9bcdf283b416fa3b47d2020f765187bcfe1c278d
Yun Chankrisna <chankrisnayun@gmail.com>
2024-07-05 14:16:29 +0700

    deleted unused files

--- index.js
+++
@@ -1,5 +0,0 @@
-// import createNotes from "./components/note-card.js";
-// import navigation from "./components/navigation-bar.js";
-// import more from "./components/add-more-buttton.js";
-// submitButton.addEventListener("click", createNotes())
-     
